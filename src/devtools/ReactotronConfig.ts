3a64029e4d264e59e6a17e5a8225cefe403185b3
Frank Calise <fcalise@gmail.com>
2023-11-07 10:57:48 -0500

    chore: clean up

--- src/devtools/ReactotronConfig.ts
+++ src/devtools/ReactotronConfig.ts
@@ -10,7 +10,7 @@ import { ArgType } from "reactotron-core-client"
 import { mst } from "reactotron-mst"
 
 import { clear } from "src/utils/storage"
-import { goBack, resetRoot, navigate } from "src/navigators/navigationUtilities"
+// import { goBack, resetRoot, navigate } from "src/navigators/navigationUtilities"
 
 import { Reactotron } from "./ReactotronClient"
 
@@ -58,41 +58,41 @@ reactotron.onCustomCommand({
   },
 })
 
-reactotron.onCustomCommand({
-  title: "Reset Navigation State",
-  description: "Resets the navigation state",
-  command: "resetNavigation",
-  handler: () => {
-    Reactotron.log("resetting navigation state")
-    resetRoot({ index: 0, routes: [] })
-  },
-})
+// reactotron.onCustomCommand({
+//   title: "Reset Navigation State",
+//   description: "Resets the navigation state",
+//   command: "resetNavigation",
+//   handler: () => {
+//     Reactotron.log("resetting navigation state")
+//     resetRoot({ index: 0, routes: [] })
+//   },
+// })
 
-reactotron.onCustomCommand<[{ name: "route"; type: ArgType.String }]>({
-  command: "navigateTo",
-  handler: (args) => {
-    const { route } = args ?? {}
-    if (route) {
-      Reactotron.log(`Navigating to: ${route}`)
-      navigate(route as any) // this should be tied to the navigator, but since this is for debugging, we can navigate to illegal routes
-    } else {
-      Reactotron.log("Could not navigate. No route provided.")
-    }
-  },
-  title: "Navigate To Screen",
-  description: "Navigates to a screen by name.",
-  args: [{ name: "route", type: ArgType.String }],
-})
+// reactotron.onCustomCommand<[{ name: "route"; type: ArgType.String }]>({
+//   command: "navigateTo",
+//   handler: (args) => {
+//     const { route } = args ?? {}
+//     if (route) {
+//       Reactotron.log(`Navigating to: ${route}`)
+//       navigate(route as any) // this should be tied to the navigator, but since this is for debugging, we can navigate to illegal routes
+//     } else {
+//       Reactotron.log("Could not navigate. No route provided.")
+//     }
+//   },
+//   title: "Navigate To Screen",
+//   description: "Navigates to a screen by name.",
+//   args: [{ name: "route", type: ArgType.String }],
+// })
 
-reactotron.onCustomCommand({
-  title: "Go Back",
-  description: "Goes back",
-  command: "goBack",
-  handler: () => {
-    Reactotron.log("Going back")
-    goBack()
-  },
-})
+// reactotron.onCustomCommand({
+//   title: "Go Back",
+//   description: "Goes back",
+//   command: "goBack",
+//   handler: () => {
+//     Reactotron.log("Going back")
+//     goBack()
+//   },
+// })
 
 /**
  * We're going to add `console.tron` to the Reactotron object.
